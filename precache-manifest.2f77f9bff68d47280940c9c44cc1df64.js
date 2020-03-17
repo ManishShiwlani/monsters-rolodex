b36696974c7bc930ffaa254cc3239c1a917b0486
manish <manishshiwlani@gmail.com>
2020-03-17 00:30:09 -0400

    Updates

---
+++ precache-manifest.2f77f9bff68d47280940c9c44cc1df64.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71b1a922b39a50abe4ebd8e47ac28689",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "f3d54970c39f05dc5a97",
+    "url": "/monsters-rolodex/static/css/main.7798b10b.chunk.css"
+  },
+  {
+    "revision": "30436d15d072549122d4",
+    "url": "/monsters-rolodex/static/js/2.0d232ef2.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters-rolodex/static/js/2.0d232ef2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3d54970c39f05dc5a97",
+    "url": "/monsters-rolodex/static/js/main.e899fa99.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
